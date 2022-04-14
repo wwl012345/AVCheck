c88f01ca46516a5542fd5fb438f0f6e259643190
Wh0isR00T <53456907+wwl012345@users.noreply.github.com>
2022-04-14 14:16:07 +0800

    Add files via upload

---
+++ AVCheck.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+banner = '''
+
+     ___   ____    ____  ______  __    __   _______   ______  __  ___ 
+    /   \  \   \  /   / /      ||  |  |  | |   ____| /      ||  |/  / 
+   /  ^  \  \   \/   / |  ,----'|  |__|  | |  |__   |  ,----'|  '  /  
+  /  /_\  \  \      /  |  |     |   __   | |   __|  |  |     |    <   
+ /  _____  \  \    /   |  `----.|  |  |  | |  |____ |  `----.|  .  \  
+/__/     \__\  \__/     \______||__|  |__| |_______| \______||__|\__\ 
+
+                                                                      
+'''
+
+print(banner)
+
+with open('tasklist.txt', 'r') as file:
+    for line in file.readlines():
+        line = line.strip('\n')
+        #取出tasklist中的进程名称
+        target = line.split(' ')[0]
+        with open('杀软识别.txt', 'r') as f:
+            for i in f.readlines():
+                #将取出的进程名与杀软识别列表中的名称进行对比
+                if target == i.strip('\n').split('\"')[1]:
+                    result = i.strip('\n').split('\"')[3]
+                    if result is None:
+                        print("没有识别到杀软或者不存在杀软")
+                    else:
+                        print("服务器上存在的杀毒软件有:" + result)
+                else:
+                    pass
